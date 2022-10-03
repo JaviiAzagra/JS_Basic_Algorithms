a0555cecd5d3e524484dd58e32105db4d2b0dd7d
JaviAzagra <javiazagra12@gmail.com>
2022-10-03 18:30:39 +0200

    1:Variables

---
+++ 1_Variables.js
@@ -0,0 +1,24 @@
+var line = "-----------"
+/* 1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella. */
+
+let myFavoriteHero = "Hulk";
+console.log(myFavoriteHero);
+
+/* 1.2 Crea una variable llamada x, asigna el valor 50 a ella. */
+
+console.log(line);
+let x = 50;
+console.log(x);
+
+/* 1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10.  */
+
+console.log(line);
+let h = 5;
+let y = 10;
+console.log("h:",h ,"y:",y);
+
+/* 1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'. */
+
+console.log(line);
+let z = h + y;
+console.log("La suma de h + y es igual a:",z); 
