97479419dd85fe81d7087f849014ad1b5181d2d5
JaviAzagra <javiazagra12@gmail.com>
2022-10-04 09:12:11 +0200

    Iteration#3

--- 3_Operadores.js
+++ 3_Operadores.js
@@ -3,24 +3,24 @@ var line = "-----------------------------------"
 /* 1.1 Multiplica 10 por 5 y muestra el resultado mediante alert. */
 
 let multiplication = 10 * 5;
-alert(multiplication);
-/* console.log("El resultado es:",multiplication); */
+/* alert(multiplication); */
+console.log("El resultado es: " + multiplication);
 
 /* 1.2 Divide 10 por 2 y muestra el resultado en un alert. */
 
 console.log(line);
 
 let division = 10 / 2;
-alert(division);
-/* console.log("El resultado es:",division); */
+/* alert(division); */
+console.log("El resultado es: " + division);
 
 /* 1.3 Muestra mediante un alert el resto de dividir 15 por 9. */
 
 console.log(line);
 
 let rest = 15 % 9;
-alert(rest);
-/* console.log("El resultado es:",rest); */
+/* alert(rest); */
+console.log("El resultado es: " + rest);
 
 /* 1.4 Usa el correcto operador de asignación que resultará en x = 15, 
 teniendo dos variables y = 10 y z = 5. */
@@ -31,8 +31,8 @@ let y = 10;
 let z = 5;
 
 let x = y + z;
-alert(x);
-/* console.log("El resultado es:",x); */
+/* alert(x); */
+console.log("El resultado es: " + x);
 
 /* 1.5 Usa el correcto operador de asignación que resultará en x = 50,
 teniendo dos variables y = 10 y z = 5. */
@@ -43,5 +43,5 @@ y = 10;
 z = 5;
 
 x = y * z;
-alert(x);
-/* console.log("El resultado es:",x); */
+/* alert(x); */
+console.log("El resultado es: " + x);
